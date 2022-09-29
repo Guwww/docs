3ac9e08b181f5dcfafd91081ba30279e2c761d81
guwei <8723460@qq.com>
2022-09-29 17:57:31 +0800

    gw819

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,4 @@
-const moment = require('moment');
+// const moment = require('moment');
 module.exports = {
   title:'狂奔的蜗牛',
   description:'飞越高山与大海的鱼',
@@ -7,19 +7,19 @@ module.exports = {
     ['meta',{name:'title',content:'飞越高山与大海的鱼'}],
     ['meta',{name:'keywords',content:'vuePress介绍，飞越高山与大海的鱼'}],
   ],
-  plugins: [
-    [
-      '@vuepress/last-updated',
-      {
-        transformer: (timestamp, lang) => {
-          // 不要忘了安装 moment
-          const moment = require('moment')
-          moment.locale(lang)
-          return moment(timestamp).format("LLLL")
-        }
-      }
-    ]
-  ],
+  // plugins: [
+  //   [
+  //     '@vuepress/last-updated',
+  //     {
+  //       transformer: (timestamp, lang) => {
+  //         // 不要忘了安装 moment
+  //         const moment = require('moment')
+  //         moment.locale(lang)
+  //         return moment(timestamp).format('LLLL');
+  //       }
+  //     }
+  //   ]
+  // ],
   themeConfig: {
     lastUpdated: '更新时间', // string | boolean
     logo: '/assets/img/logo.png',
